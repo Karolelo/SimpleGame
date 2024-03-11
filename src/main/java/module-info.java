cbdfea15e140dbb5ba470b664180657d7c5d26ed
Karol Niekrasz <karolniekrasz@gmail.com>
2024-03-11 11:47:59 +0100

    delete module file

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.simplegame {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens org.example.simplegame to javafx.fxml;
-    exports org.example.simplegame;
-}
